fe87eb234b78546553b43789085b3b543e3485f5
MUSHFIQUER-RAKIT <mushfiquerrakit@gmail.com>
2024-10-10 16:58:22 +0600

    Added History

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,18 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-export default function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
+function Board({ xIsNext, squares, onPlay }) {
+  const winner = calculateWinner(squares);
+  let status;
+
+  if (winner) {
+    status = `Winner is: ${winner}`;
+  } else {
+    status = "Next Player IS:" + (xIsNext ? "X" : "O");
+  }
 
   function handClick(i) {
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
 
@@ -26,11 +32,11 @@ export default function Board() {
     } else {
       nextSquares[i] = "O";
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
   return (
     <>
+      <div>{status}</div>
       <div className="flex">
         <Square value={squares[0]} onSquareClick={() => handClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handClick(1)} />
@@ -48,4 +54,65 @@ export default function Board() {
       </div>
     </>
   );
+}
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [xIsNext, setXIsNext] = useState(true);
+  const [currentMove, setCurrentMove] = useState(0);
+
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    setXIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(move) {
+    setCurrentMove(move);
+    setXIsNext(move % 2 === 0);
+  }
+  const moves = history.map((squares, move) => {
+    let descripction;
+    if (move > 0) {
+      descripction = `Go to the move # ${move}`;
+    } else {
+      descripction = "Start The Game";
+    }
+    return (
+      <li className="mb-2" key={move}>
+        <button onClick={() => jumpTo(move)}>{descripction}</button>
+      </li>
+    );
+  });
+  return (
+    <div className="flex justify-center mt-6 p-8">
+      <div className="p-8">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="p-8">
+        <ol className="border border-gray-400  text-lg">{moves}</ol>
+      </div>
+    </div>
+  );
+}
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
